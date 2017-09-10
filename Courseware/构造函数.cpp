da028c1a3b11e05f2cfb59ed9a438014314770d1
VitozzzZ <zealucius@gmail.com>
2017-09-10 09:27:28 +0800

    Update 构造函数.cpp

--- Courseware/构造函数.cpp
+++ Courseware/构造函数.cpp
@@ -22,3 +22,66 @@ int main(){
     delete []array4;
     return 0;
 }
+
+
+//复制构造函数
+//系统默认
+class Complex{
+private:
+    double real,imag;
+};
+Complex c1;//调用缺省无参构造函数
+Complex c2(c1);//调用缺省复制构造函数，将c2初始化成c1一样 等价于Complex c2=c1(初始化语句非赋值语句）
+
+//自己编写
+class Complex{
+public:
+    double real,imag;
+Complex(){}
+Complex(const Complex&c){
+    real=c.real;
+    imag=c.imag;
+    cout<<"Copy Constructor called";
+}
+    
+};
+Complex c1;
+Complex c2(c1);//输出Copy Constructor called
+
+/*复制构造函数起作用的三种情况
+1.用一个对象去初始化同类的另一个对象时
+Complex c2(c1) 或者 Complex c2=c1
+2.如果某函数有一个参数是类A的对象，调用该函数时，类A的复制构造函数也会被调用，去初始化这个参数*/
+class A{
+public:
+    A(){};
+    A(A&a){
+        cout<<"Copy constructor called"<<endl;
+    }
+};
+void Func(A a1){}
+int main(){
+    A a2;
+    Func(a2);//输出Copy constructor called
+    return 0;
+}
+//3.如果函数的返回值是类A的对象，则函数返回时，A的复制构造函数会被调用，去初始化以返回值存在的类A的对象
+#include <iostream>
+using namespace std;
+class A{
+public:
+    int v;
+    A(int n){v=n;}
+    A(const A&a){
+        v=a.v;
+        cout<<"Copy constructor called"<<endl;
+    }
+};
+A Func(){
+    A b(4);
+    return b;
+}
+int main(){
+    cout<<Func().v<<endl;
+    return 0;
+}
