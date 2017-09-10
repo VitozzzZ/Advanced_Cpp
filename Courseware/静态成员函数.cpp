fb2f77d371646e7bbc1291b221beb6ce07433f80
VitozzzZ <zealucius@gmail.com>
2017-09-10 11:17:50 +0800

    Create 静态成员函数.cpp

---
+++ Courseware/静态成员函数.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+class CRectangle{
+private:
+    int w,h;
+    static int nTotalArea;
+    static int nTotalNumber;
+public:
+    CRectangle(int w_,int h_);
+    CRectangle(CRectangle&r){
+        w=r.w,h=r.h;
+        nTotalNumber++;
+        nTotalArea+=w*h;
+    }
+
+    ~CRectangle();
+    static void PrintTotal();
+};
+CRectangle::CRectangle(int w_,int h_){
+    w=w_;
+    h=h_;
+    nTotalNumber++;
+    nTotalArea+=w*h;
+}
+CRectangle::~CRectangle(){
+    nTotalNumber--;
+    nTotalArea-=w*h;
+}
+void CRectangle::PrintTotal(){
+    cout<<nTotalNumber<<","<<nTotalArea<<endl;
+}
+int CRectangle::nTotalNumber=0;
+int CRectangle::nTotalArea=0;
+int main(){
+    CRectangle r1(3,3),r2(2,2);
+    CRectangle::PrintTotal();
+    r1.PrintTotal();
+    return 0;
+}
