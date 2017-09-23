be3f407095b13f96ef0bd80c03ac2a22c20e37e2
VitozzzZ <zealucius@gmail.com>
2017-09-23 09:28:02 +0800

    Update 赋值运算符重载.cpp

--- Courseware/Week4/赋值运算符重载.cpp
+++ Courseware/Week4/赋值运算符重载.cpp
@@ -9,7 +9,7 @@ public:
     char* operator =(const char* s);
     ~String();
 };
-char* String::operator=(const char *s){
+char* String::operator=(const char *s){    //浅拷贝
     if(str)
         delete []str;
     if(s){
@@ -30,4 +30,19 @@ int main(){
     s="zsq";
     cout<<s.c_str()<<endl;
     return 0;
+}
+
+
+String & String::operator =(const String &s){   //深拷贝
+    if(str==s.str)
+        return *this;
+    if(str)
+        delete []str;
+    if(s.str){
+        str=new char[strlen(s.str)+1];
+        strcpy(str,s,str);
+    }
+    else
+        str=NULL;
+    return *this;
 }
