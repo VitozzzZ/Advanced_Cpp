34cae52ca15721aacc0182168b964927a120a666
VitozzzZ <zealucius@gmail.com>
2017-09-29 02:14:46 -0500

    Update 多态与虚函数.cpp

--- Courseware/Week6/多态与虚函数.cpp
+++ Courseware/Week6/多态与虚函数.cpp
@@ -36,4 +36,23 @@ int main() {
     r.SomeVirtualFunction(); //调用哪个虚函数取决于r引用哪种类型的对象 此处调用派生类虚函数
     return 0;
 }
- 
+
+
+//用法三
+#include <iostream>
+using namespace std;
+class Base {
+public:
+    void fun1() { fun2(); }
+    virtual void fun2() { cout << "Base::fun2()" << endl; }
+};
+class Derived:public Base {
+public:
+    virtual void fun2() { cout << "Derived:fun2()" << endl; }
+};
+int main() {
+    Derived d;
+    Base * pBase = & d;
+    pBase->fun1();    //在非构造函数，非析构函数的成员函数中调用虚函数，是多态!!!
+    return 0;         //会输出Derived:fun2()
+}
