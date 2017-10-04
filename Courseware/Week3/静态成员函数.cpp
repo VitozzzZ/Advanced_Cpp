04a816a084b4b49ae53f8589b8ae371811f9c7dd
VitozzzZ <zealucius@gmail.com>
2017-10-03 22:00:48 -0500

    Update 静态成员函数.cpp

--- Courseware/Week3/静态成员函数.cpp
+++ Courseware/Week3/静态成员函数.cpp
@@ -7,8 +7,9 @@ private:
     static int nTotalNumber;
 public:
     CRectangle(int w_,int h_);
-    CRectangle(CRectangle&r){
-        w=r.w,h=r.h;
+    CRectangle(CRectangle&r){   //复制构造函数
+        w=r.w;
+        h=r.h;
         nTotalNumber++;
         nTotalArea+=w*h;
     }
@@ -30,10 +31,17 @@ void CRectangle::PrintTotal(){
     cout<<nTotalNumber<<","<<nTotalArea<<endl;
 }
 int CRectangle::nTotalNumber=0;
-int CRectangle::nTotalArea=0;
+int CRectangle::nTotalArea=0;  // 必须在定义类的文件中对静态成员变量进行一次说明或初始化。否则编译能通过，链接不能通过。
 int main(){
     CRectangle r1(3,3),r2(2,2);
     CRectangle::PrintTotal();
     r1.PrintTotal();
+/*****
+ 如何访问静态成员
+1) 类名::成员名  CRectangle::PrintTotal();
+2) 对象名.成员名 CRectangle r; r.PrintTotal(); 
+3) 指针->成员名  CRectangle * p = &r; p->PrintTotal(); 
+4) 引用.成员名   CRectangle & ref = r; int n = ref.nTotalNumber;
+*****/
     return 0;
 }
