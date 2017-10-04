01c1610e21445df1a4d702e219c8b6a1f4c0ef4b
VitozzzZ <zealucius@gmail.com>
2017-10-04 08:36:57 -0500

    Update 赋值运算符重载.cpp

--- Courseware/Week4/赋值运算符重载.cpp
+++ Courseware/Week4/赋值运算符重载.cpp
@@ -49,3 +49,53 @@ String & String::operator =(const String &s){   //深拷贝
         str=NULL;
     return *this;
 }
+/*****
+为什么是错的？？？？？
+*****/
+#include <iostream>
+#include <string.h>
+class String{
+private:
+    char *str;
+public:
+    String():str(NULL){}     //构造函数 初始化str为NULL
+    String(String & s);      //复制构造函数
+    const char*c_str(){return str;}
+    String & operator =(const String &s);
+    ~String();
+};
+String::String(String & s)
+{
+    if(s.str) {
+        str = new char[strlen(s.str)+1];
+        strcpy(str, s.str);
+    }
+    else
+        str = NULL;
+    
+}
+String & String::operator =(const String &s){   //深拷贝
+    if(str==s.str)    //解决s=s的bug
+        return *this;
+    if(str)
+        delete []str;
+    if(s.str){
+        str=new char[strlen(s.str)+1];
+        strcpy(str,s.str);
+    }
+    else
+        str=NULL;
+    return *this;
+}
+
+String::~String(){
+    if(str) delete []str;
+}
+int main(){
+    String s1,s2;
+    s1="zzz";
+    s2="zsq";
+    printf("%s", s1.c_str());
+    printf("%s", s2.c_str());
+    return 0;
+}
