33f83c50331624956378ec15b6f8ea4a61c0d41c
VitozzzZ <zealucius@gmail.com>
2017-10-03 21:15:26 -0500

    Update const and &.cpp

--- Courseware/Week2/const and &.cpp
+++ Courseware/Week2/const and &.cpp
@@ -19,9 +19,10 @@ int main()
     return 0;
 }
 
-/*  对&和const的彻底理解
+/*****
+对&和const的彻底理解
 参数中
-1.第六行处 string &str 若不加&的话，str则被复制一份，函数中对str的操作实质上是对其复制品的操作，渊str的值并不会改变。
+1.第六行处 string &str 若不加&的话，str则被复制一份，函数中对str的操作实质上是对其复制品的操作，原str的值并不会改变。
   就本题来说的话，输出2行a value
 2.加&的话，传入的str即是调用层的实际参数，这样省去了复制过程，效率会提高，并且如果函数中修改了str，str就会变。
 3.如果变成 const string &str，编译出错，因为const类型的str不可以作为函数返回值（const类型不可以赋值给非const）
@@ -30,3 +31,4 @@ int main()
 2.char get_val不能作为左值，函数会返回一个str[ix]的复制品
 3.const char &get_val同样不能作为左值因为加了const限定不能被修改，但不会产生str[ix]的复制
 4.const char get_val 不能作为左值 函数返回str[ix]的复制但不能被修改
+*****/
